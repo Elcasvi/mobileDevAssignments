29fd2b98efd330bb175fbe4b5c862a85780e85cd
Elcasvi <casvi.sanchez@gmail.com>
2023-08-30 13:17:37 -0600

    I keep working on the get method in the MenuActivity

--- Android/TheVetApp/app/src/main/java/com/example/thevetapp/MenuActivity.kt
+++ Android/TheVetApp/app/src/main/java/com/example/thevetapp/MenuActivity.kt
@@ -1,16 +1,16 @@
 package com.example.thevetapp
 
 import android.content.ContentValues
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,23 +18,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.thevetapp.ui.theme.TheVetAppTheme
+import com.google.firebase.firestore.auth.User
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class MenuActivity : ComponentActivity() {
 
-    data class AnimalInfo(
-        var animalList:ArrayList<AnimalItem> = arrayListOf()
-    )
+class MenuActivity : ComponentActivity() {
     data class AnimalItem(
         var race: String = "",
         var name: String = "",
-        var age: Int = 0,
+        var age: String = "",
         var weight: String = ""
     )
 
     private val db = Firebase.firestore
-    lateinit var animalInfo:AnimalInfo
+
+    val animalList:MutableList<AnimalItem> = arrayListOf()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -44,69 +45,77 @@ class MenuActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //ListOfAnimals()
+                    ListOfAnimals()
+                    //MyColumnWithList(animalList)
                 }
             }
-
-            }
+        }
         //Get
         db.collection("animals")
             .get()
-            .addOnSuccessListener { document  ->
-                try {
-                    if (document != null) {
-                        //animalInfo = document.toObject(AnimalInfo::class.java) ?: AnimalInfo()
-                        Toast.makeText(
-                            this@MenuActivity,
-                            "DocumentSnapshot read successfully!",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    } else {
-                        Toast.makeText(
-                            this@MenuActivity,
-                            "No such document!",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                } catch (ex: Exception) {
-                    ex.message?.let { Log.e(TAG, it) }
+            .addOnSuccessListener { result ->
+            for (document in result) {
+                val animalItem = AnimalItem()
+                Log.d(ContentValues.TAG, "${document.id} => ${document.data}")
+                val animalRace:String = document.data.get("race").toString()
+                val animalName:String = document.data.get("name").toString()
+                val animalAge:String = document.data.get("age").toString()
+                val animalWeight:String = document.data.get("weight").toString()
+
+                animalItem.race=animalRace
+                animalItem.name=animalName
+                animalItem.age=animalAge
+                animalItem.weight=animalWeight
+                animalList.add(animalItem)
+            }
+                /*
+                for (animal in animalList){
+                    Log.d("Tag","race: ${animal.race}")
+                    Log.d("Tag","name: ${animal.name}")
+                    Log.d("Tag","age: ${animal.age}")
+                    Log.d("Tag","weight: ${animal.weight}")
                 }
 
+                 */
 
-            }
-            .addOnFailureListener { exception ->
-                Log.w(ContentValues.TAG, "Error getting documents.", exception)
-            }
-    }
-
-
-    @Composable
-    fun ListOfAnimals() {
-
-        Column {
-            animalInfo.animalList.forEach { animal ->
-                AnimalRow(animal)
-            }
         }
     }
 
     @Composable
     fun AnimalRow(animal: MenuActivity.AnimalItem) {
-        Text(animal.name)
+        Text(text = animal.name)
     }
 
-    @Composable
-    fun FlexCols()
-    {
-        Row(horizontalArrangement = Arrangement.Center) {
-            Column {
 
+    @Composable
+    fun ListOfAnimals() {
+        Column {
+            Button(onClick = {
+            for (animal in animalList){
+                Log.d("Tag","race: ${animal.race}")
+                Log.d("Tag","name: ${animal.name}")
+                Log.d("Tag","age: ${animal.age}")
+                Log.d("Tag","weight: ${animal.weight}")}
+            }) {
 
             }
         }
     }
 
+    @Composable
+    fun MyColumnWithList(items: List<AnimalItem>) {
+        LazyColumn {
+            items(items) { item ->
+                AnimalRow(animal = item)
+            }
+        }
+    }
+
+
 }
+
+
+
 
 
 
